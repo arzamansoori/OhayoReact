a9ca81b07887c4d1fc07d14b87636af1b461e58d
arzamansoori <arzamansoori786@gmail.com>
2024-03-10 20:30:06 +0530

    adding episode first

--- Ep.1Inception/app.js
+++ Ep.1Inception/app.js
@@ -1,3 +1,23 @@
+import React from "react"
+import ReactDOM from "react-dom/client"
+
+//COMPONENT COMPOSITION
+const Title = () => (
+  <h1 className="heading">Namaste React Title</h1>  
+);
+
+const HeadingCompTitle = () => (
+    <div>
+    <Title />
+    <h1>Hello from heading component</h1>
+    </div>
+);
+
+const headRoot = ReactDOM.createRoot(document.getElementById("heading"));
+
+headRoot.render(<HeadingCompTitle />);
+
+
 // using html code
 /* <div>
     <div>
@@ -34,15 +54,25 @@
 
 const parent = React.createElement('div', {id : "parent"}, 
                [React.createElement("div", {id: "cild1"}, 
-               [React.createElement("h3", {}, "I am an h3 tag"),
+               [React.createElement("h3", {}, "I am learning react 🚀"),
                React.createElement("h4", {}, "I am an h4 tag")]),
                React.createElement("div", {id: "cild2"}, 
                [React.createElement("h3", {}, "I am an h3 tag"),
                React.createElement("h4", {}, "I am an h4 tag")])]);
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
+//const root = ReactDOM.createRoot(document.getElementById("root"));
+// console.log(parent);
 
-root.render(parent);
+// //JSX: Html like syntax but not html
+// //JSX => react.createElement => ReactElement-JS Object => HTML Element
+const jsxHeading = <h1 className="hello" id="heading"> Hello from the JSX world</h1>
+console.log(jsxHeading)
+
+const rootJSX = ReactDOM.createRoot(document.getElementById("root"));
+rootJSX.render(jsxHeading);
+
+// //root.render(parent);
+
 
 
 
